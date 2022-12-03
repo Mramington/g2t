e4b4a548e31b37e86ff593ba65cd3239e2692b97
Cocojambo17 <Anton17_00@mail.ru>
2022-12-03 19:19:16 +0300

    ИЗМЕНЕНИЯ
    ИЗМЕНЕНИЯ

--- example1.py
+++ example1.py
@@ -2,13 +2,16 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 
 # TODO Сделать через класс
+class TODO(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.resize(250, 150)
+        self.move(300, 300)
+        self.setWindowTitle('Hello world')
+        self.show()
 if __name__ == '__main__':
     app = QApplication(sys.argv) # Параметр sys.argv — это список аргументов из командной строки.
-    window = QWidget() # Виджет QWidget — это основной класс всех объектов пользовательского интерфейса в PyQt5.
-    window.resize(250, 150)# Метод resize() изменяет размер виджета. Здесь задана ширина 250px и высота 150px.
-    window.move(300, 300)# Метод move() перемещает виджет на позицию с координатами x = 300 и y = 300 на экране.
-    window.setWindowTitle('Hello world')# Здесь мы устанавливаем название нашего окна. Оно показывается в строке заголовка.
-    window.show()# Метод show() отображает виджет на экране. Виджет сначала создаётся в памятии позже показывается на экране.
+    a = TODO()
     sys.exit(app.exec_())# Наконец, мы входим в главный цикл приложения.
 
 
